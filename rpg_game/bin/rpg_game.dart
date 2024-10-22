5c02e6e4ad71c9ad2b12463926f70f11db8f672e
Rennan-Dev <rennandev7@gmail.com>
2024-10-22 11:47:26 -0400

    feat: menu adicionado

--- rpg_game/bin/rpg_game.dart
+++ rpg_game/bin/rpg_game.dart
@@ -1,19 +1,51 @@
+import 'dart:io';
+
 void main() {
+  List<Guerreiro> guerreiros = [];
+  List<Mago> magos = [];
 
-  Personagem personagem1 = new Personagem('Rennan', 'Humano', 'Ladrão', 24, 100, 100, 1.74, false, ['Ataque rápido', 'Ataque furtivo aumenta o dano em 3x', 'Ao eliminar inimigos furtivamente recebe uma recompensa maior em xp', 'Golpe com adagas dão o dobro de dano']);
-  print(personagem1.exibirFicha());
+  print('\n');
+  print('''  
+██████╗░██████╗░░██████╗░  ░██████╗░░█████╗░███╗░░░███╗███████╗
+██╔══██╗██╔══██╗██╔════╝░  ██╔════╝░██╔══██╗████╗░████║██╔════╝
+██████╔╝██████╔╝██║░░██╗░  ██║░░██╗░███████║██╔████╔██║█████╗░░
+██╔══██╗██╔═══╝░██║░░╚██╗  ██║░░╚██╗██╔══██║██║╚██╔╝██║██╔══╝░░
+██║░░██║██║░░░░░╚██████╔╝  ╚██████╔╝██║░░██║██║░╚═╝░██║███████╗
+╚═╝░░╚═╝╚═╝░░░░░░╚═════╝░  ░╚═════╝░╚═╝░░╚═╝╚═╝░░░░░╚═╝╚══════╝
+''');
 
-  Guerreiro guerreiro1 = new Guerreiro('João', 'Humano', 26, 100, 100, 1.69, false, ['Ataque forte', 'Quanto menos vida o guerreiro tiver, mais dano de ataque ele possui', 'Ao eliminar inimigos recebe uma recompensa maior em xp', 'Golpe com espada longa dão o dobro de dano']);
-  print(guerreiro1.exibirFicha());
-
-  Mago mago1 = new Mago('Apollo', 'Humano', 7, 100, 100, 0.40, true, ['Ataque mágico', 'Possui recuperação de vida uma vez durante combate', 'Agachar deixa o mago invisível', 'Usar o Lançar Feitiço te gera desgaste e o mago perde 10 pontos de energia.']);
-  print(mago1.exibirFicha());
-  mago1.lancarFeitico();
-
-  guerreiro1.atacar(personagem1);
-  mago1.atacar(personagem1);
+  int opcao;
+  do {
+    opcao = menu_inicial();
+    if(opcao == 1) {
+      criar_personagem(guerreiros, magos);
+    }else if(opcao == 2) {
+      mostrar_personagens(guerreiros, magos);
+    }else if(opcao == 3) {
+      fazer_feitico(magos);
+    }else if(opcao ==4) {
+      atacar(guerreiros, magos);
+    }
+  } while (opcao != 5);
 }
 
+int menu_inicial() {
+  int res = 0;
+  print('------------------------------------------------------------------------------------');
+  print('\nMenu Inicial');
+  print('1 - Criar personagem');
+  print('2 - Visualizar personagens');
+  print('3 - Fazer um feitiço com um mago');
+  print('4 - Atacar um personagem');
+  print('5 - Sair da Aplicação');
+  print('\nResposta: ');
+
+  String? input = stdin.readLineSync(); 
+  if (input != null) {
+    res = int.tryParse(input) ?? 0; 
+  }
+  return res; 
+}
 
 class Personagem {
   String nome, raca, classe;
@@ -25,8 +57,7 @@ class Personagem {
   Personagem(this.nome, this.raca, this.classe, this.idade, this.vida, this.energia, this.altura, this.isMagico, this.habilidades);
 
   String exibirFicha() {
-    print('------------------------------------------------------------------------------------');
-    return '\nCaracterísticas do personagem=\n\n'
+    return '\n'
            'Nome: $nome\nRaça: $raca\nClasse: $classe\n'
            'Idade: $idade\nVida: $vida\nEnergia: $energia\n'
            'Altura: $altura\nPossui Magia? $isMagico\n'
@@ -51,7 +82,7 @@ class Guerreiro extends Personagem implements Combate {
       print('\nCombate acontecendo');
       print('$nome atacando ${personagem.nome}');
       personagem.vida -= 15;
-      print('Dano de ataque: 10\nVida atual de ${personagem.nome}: ${personagem.vida}');
+      print('Dano de ataque: 15\nVida atual de ${personagem.nome}: ${personagem.vida}');
       print(
         "  ,   A           {}\n"
         " / \\, | ,        .--.\n"
@@ -158,10 +189,149 @@ abstract class Combate {
   void atacar(Personagem personagem);
 }
 
+void criar_personagem(List<Guerreiro> guerreiros, List<Mago> magos) {
+  print('------------------------------------------------------------------------------------');
 
+  String nome = lerEntrada('Digite o nome do personagem: ');
+  if (guerreiros.any((guerreiro) => guerreiro.nome.toLowerCase() == nome.toLowerCase()) || 
+      magos.any((mago) => mago.nome.toLowerCase() == nome.toLowerCase())) {
+    print('Nome de personagem já existe. Por favor, escolha um nome diferente.');
+    return; 
+  }
 
+  String raca = lerEntrada('Digite a raça do personagem: ');
+  int idade = int.parse(lerEntrada('Digite a idade do personagem: '));
+  double altura = double.parse(lerEntrada('Digite a altura do personagem: '));
+  String classe = lerEntrada('Você quer ser um Guerreiro ou Mago? (digite "guerreiro" ou "mago"): ');
 
+  // Verifica a escolha do usuário
+  if (classe.toLowerCase() == 'guerreiro') {
+    List<String> habilidades = ['Ataque físico', 'Quanto menos vida o guerreiro tiver, mais dano de ataque ele possui', 'Ao eliminar inimigos recebe uma recompensa maior em xp', 'Golpe com espada longa dão o dobro de dano'];
+    Guerreiro guerreiro = new Guerreiro(nome, raca, idade, 100, 100, altura, false, habilidades);
+    guerreiros.add(guerreiro);
+    print('\nGuerreiro criado:\n${guerreiro.exibirFicha()}');
 
+  } else if (classe.toLowerCase() == 'mago') {
+    List<String> habilidades = ['Ataque mágico', 'Possui recuperação de vida uma vez durante combate', 'Agachar deixa o mago invisível', 'Usar o Lançar Feitiço te gera desgaste e o mago perde 10 pontos de energia.'];
+    Mago mago = new Mago(nome, raca, idade, 100, 100, altura, true, habilidades);
+    magos.add(mago);
+    print('\nMago criado:\n${mago.exibirFicha()}');
+  } else {
+    print('\nRaça desconhecida. O personagem não foi criado.');
+  }
+}
 
+String lerEntrada(String prompt) {
+  String? input;
+  do {
+    stdout.write(prompt); // O `stdout.write` coloca o cursor ao lado da pergunta
+    input = stdin.readLineSync();
+    if (input == null || input.isEmpty) {
+      print('Entrada inválida. Por favor, tente novamente.');
+    }
+  } while (input == null || input.isEmpty); // Loop até receber uma entrada válida
+  return input;
+}
 
+void mostrar_personagens(List<Guerreiro> guerreiros, List<Mago> magos) {
+  print('------------------------------------------------------------------------------------');
+  print('\nGuerreiros:\n');
+  for(Guerreiro guerreiro in guerreiros) {
+    print(guerreiro.exibirFicha());
+  }
 
+  print('\nMagos:\n');
+  for(Mago mago in magos) {
+    print(mago.exibirFicha());
+  }
+} 
+
+void fazer_feitico(List<Mago> magos) {
+  print('------------------------------------------------------------------------------------');
+  print('\nFazer magia\n');
+  String nome = lerEntrada('Digite o nome do personagem: ');
+
+  Mago? mago = magos.firstWhere(
+    (m) => m.nome.toLowerCase() == nome.toLowerCase(),
+    orElse: () => Mago('','',0,0,0,0,false,[])
+  );
+
+  if (mago.nome.isEmpty) {
+    print('Personagem não existe! Tente com outro');
+  } else {
+    if (mago.energia >= 10) {
+      mago.lancarFeitico(); 
+    } else {
+      print('Energia insuficiente para lançar o feitiço. Energia atual: ${mago.energia}');
+    }
+  }
+}
+
+void atacar(List<Guerreiro> guerreiros, List<Mago> magos) {
+  print('------------------------------------------------------------------------------------');
+  print('Escolha um personagem para atacar:\n');
+
+  for (int i = 0; i < guerreiros.length; i++) {
+    print('${i + 1}. ${guerreiros[i].nome} - Vida: ${guerreiros[i].vida} (Guerreiro)');
+  }
+
+  for (int i = 0; i < magos.length; i++) {
+    print('${i + 1 + guerreiros.length}. ${magos[i].nome} - Vida: ${magos[i].vida} (Mago)');
+  }
+
+  int escolhaAtacante = int.parse(lerEntrada('Escolha o número do personagem que atacará: ')) - 1;
+
+  if (escolhaAtacante < 0 || escolhaAtacante >= (guerreiros.length + magos.length)) {
+    print('Escolha inválida.');
+    return;
+  }
+
+  // Determina o atacante
+  dynamic atacanteEscolhido;
+  if (escolhaAtacante < guerreiros.length) {
+    atacanteEscolhido = guerreiros[escolhaAtacante];
+  } else {
+    atacanteEscolhido = magos[escolhaAtacante - guerreiros.length];
+  }
+
+  print('------------------------------------------------------------------------------------');
+  print('Escolha um personagem para ser atacado:\n');
+
+  for (int i = 0; i < guerreiros.length; i++) {
+    if (i != escolhaAtacante) {
+      print('${i + 1}. ${guerreiros[i].nome} - Vida: ${guerreiros[i].vida} (Guerreiro)');
+    }
+  }
+
+  for (int i = 0; i < magos.length; i++) {
+    if (guerreiros.length + i != escolhaAtacante) {
+      print('${i + 1 + guerreiros.length}. ${magos[i].nome} - Vida: ${magos[i].vida} (Mago)');
+    }
+  }
+
+  int escolhaDefensor = int.parse(lerEntrada('Escolha o número do personagem a ser atacado: ')) - 1;
+
+  if (escolhaDefensor < 0 || escolhaDefensor >= (guerreiros.length + magos.length) || escolhaDefensor == escolhaAtacante) {
+    print('Escolha inválida.');
+    return;
+  }
+
+  dynamic defensorEscolhido;
+  if (escolhaDefensor < guerreiros.length) {
+    defensorEscolhido = guerreiros[escolhaDefensor];
+  } else {
+    defensorEscolhido = magos[escolhaDefensor - guerreiros.length];
+  }
+
+  if (atacanteEscolhido.vida <= 0) {
+    print('${atacanteEscolhido.nome} não pode atacar porque está sem vida.');
+  } else if (defensorEscolhido.vida <= 0) {
+    print('${defensorEscolhido.nome} não pode ser atacado porque está sem vida.');
+  } else {
+    if (atacanteEscolhido is Guerreiro) {
+      atacanteEscolhido.atacar(defensorEscolhido);
+    } else if (atacanteEscolhido is Mago) {
+      atacanteEscolhido.atacar(defensorEscolhido);
+    }
+  }
+}
